f4a86268ffea964f014263f9421e81d2c4327d16
KimJiHu0 <65142418+KimJiHu0@users.noreply.github.com>
2020-08-19 12:06:12 +0900

    01 Python Start

---
+++ start.py
@@ -0,0 +1,27 @@
+'''
+ : 문자열을 의미해.
+REPL : Read Eval Print Loop
+Interactive shall(대화형 프로그래밍 환경)
+'''
+
+#파이썬은 모든 변수들은 객체라서 싱글쿼테이션이던 더블쿼테이션이든 상관이 없어.
+
+# 주석
+# 변수 = 값 형태로 사용가능해.
+a = 10
+b = 20
+
+
+# 출력하는 명령어
+print(a)
+print(b)
+print(a + b)
+
+
+# 문자 출력하는 명령어
+print('a')
+print("b")
+print("a" + 'b' + 'cd')
+
+#문자열 a와 숫자형 10을 더해준다.
+print('a' + a)
